2da51dd11606cfba335ea54b0d04e64826b8fcdf
Muthukumar <52486794+Muthukumar-sys@users.noreply.github.com>
2024-03-18 08:57:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-# main.tf
+provider "null" {}
 
-resource "tfe_organization" "test-organization" {
-  name  = "my-org-name"
-  email = "admin@company.com"
-}
-
-resource "tfe_workspace" "test" {
-  name         = "my-workspace-name"
-  organization = tfe_organization.test-organization.name
-  tag_names    = ["test", "app"]
-}
+# No resources here, just the provider configuration.
