cb45843ab53cd53e2bc41f5b92d5473f5fb27f72
Muthukumar <52486794+Muthukumar-sys@users.noreply.github.com>
2024-03-18 09:37:39 +0530

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,16 +1,70 @@
-resource "tfe_workspace" "test" {
-  name         = "UAT"
-  organization = tfe_organization.opswork
-  tag_names    = ["uat", "app"]
-}
-resource "tfe_workspace" "DEV" {
-  name         = "DEV"
-  organization = tfe_organization.opswork
-  tag_names    = ["dev", "app"]
-}
-resource "tfe_workspace" "PROD" {
-  name         = "PROD"
-  organization = tfe_organization.opswork
-  tag_names    = ["prod", "app"]
+resource "tfe_organization" "test-organization" {
+  name  = "opswork"
+  email = "admin@company.com"
 }
 
+resource "tfe_oauth_client" "test" {
+  organization     = tfe_organization.test_organization.name
+  api_url          = "https://api.github.com"
+  http_url         = "https://github.com"
+  oauth_token      =  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
+  service_provider = "github"
+}
+
+resource "tfe_workspace" "parent" {
+  name                 = "parent-ws"
+  organization         = tfe_organization.test-organization
+  queue_all_runs       = false
+  vcs_repo {
+    branch             = "main"
+    identifier         = "my-org-name/vcs-repository"
+    oauth_token_id     = tfe_oauth_client.test.oauth_token_id
+  }
+}
+
+resource "tfe_workspace" "child" {
+  name                 = "child-ws"
+  organization         = tfe_organization.test-organization
+  queue_all_runs       = false
+  vcs_repo {
+    branch             = "main"
+    identifier         = "my-org-name/vcs-repository"
+    oauth_token_id     = tfe_oauth_client.test.oauth_token_id
+  }
+}
+
+resource "tfe_workspace_run" "ws_run_parent" {
+  workspace_id    = tfe_workspace.parent.id
+
+  apply {
+    manual_confirm    = false
+    wait_for_run      = true
+    retry_attempts    = 5
+    retry_backoff_min = 5
+  }
+
+  destroy {
+    manual_confirm    = false
+    wait_for_run      = true
+    retry_attempts    = 3
+    retry_backoff_min = 10
+  }
+}
+
+resource "tfe_workspace_run" "ws_run_child" {
+  workspace_id    = tfe_workspace.child.id
+  depends_on   = [tfe_workspace_run.ws_run_parent]
+
+  apply {
+    manual_confirm    = false
+    retry_attempts    = 5
+    retry_backoff_min = 5
+  }
+
+  destroy {
+    manual_confirm    = false
+    wait_for_run      = true
+    retry_attempts    = 3
+    retry_backoff_min = 10
+  }
+}
