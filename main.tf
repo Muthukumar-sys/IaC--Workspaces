24a1561e3b9e814996d378f6b695ea7a1ca3c528
Muthukumar <52486794+Muthukumar-sys@users.noreply.github.com>
2024-03-18 08:44:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 # main.tf
 
-module "azure_workspaces" {
-  source = "./modules/azure_workspaces"
-  for_each = var.workspace_configurations
+resource "tfe_organization" "test-organization" {
+  name  = "my-org-name"
+  email = "admin@company.com"
+}
 
-  organization_name    = var.organization_name
-  workspace_name       = each.key
-  auto_apply           = each.value.auto_apply
-  queue_all_runs       = each.value.queue_all_runs
-  azure_subscription_id = var.azure_subscription_id
-  azure_region          = var.azure_region
+resource "tfe_workspace" "test" {
+  name         = "my-workspace-name"
+  organization = tfe_organization.test-organization.name
+  tag_names    = ["test", "app"]
 }
