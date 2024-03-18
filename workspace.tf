c9c60e2eabbb21e59ba80cf855baa034bdb46146
Muthukumar <52486794+Muthukumar-sys@users.noreply.github.com>
2024-03-18 09:14:19 +0530

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,10 +1,16 @@
-resource "tfe_organization" "test-organization" {
-  name  = "my-org-name"
-  email = "admin@company.com"
+resource "tfe_workspace" "test" {
+  name         = "UAT"
+  organization = tfe_organization.test-organization.name
+  tag_names    = ["UAT", "app"]
+}
+resource "tfe_workspace" "DEV" {
+  name         = "DEV"
+  organization = tfe_organization.test-organization.name
+  tag_names    = ["DEV", "app"]
+}
+resource "tfe_workspace" "PROD" {
+  name         = "PROD"
+  organization = tfe_organization.test-organization.name
+  tag_names    = ["PROD", "app"]
 }
 
-resource "tfe_workspace" "test" {
-  name         = "my-workspace-name"
-  organization = tfe_organization.test-organization.name
-  tag_names    = ["test", "app"]
-}
