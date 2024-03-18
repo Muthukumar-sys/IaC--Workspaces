7f21e0cff0db37198748c07d5095b621a846d094
Muthukumar <52486794+Muthukumar-sys@users.noreply.github.com>
2024-03-18 11:41:24 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_version = "1.6.5"
 
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.51.1"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "1.0.0"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Opswork"
+
+    workspaces {
+      name = "tfe-workspaces-NEW"
+    }
+  }
+}
